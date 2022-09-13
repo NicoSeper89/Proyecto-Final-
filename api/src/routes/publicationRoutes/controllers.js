e28634693ebeac976911d279bc789e299049dd77
thomneuhaus2 <thom.neuhaus2@gmail.com>
2022-09-13 20:55:22 -0300

    rutas depublis

---
+++ api/src/routes/publicationRoutes/controllers.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const { Publication, Property, Service, TypeOfProp, City, PropetyImage, Report } = require('../../db')
+
+//habria que agregar FilterByParams que recibe publicaciones y filtros y devuelve las
+//publis correspondientes. Si no se puede hacer flexible, habria que hacer un Filter por criterio
+// y en el llamado desde la ruta, hacer un loop que llame a todas
+//habria que agregar un sort que recibe las publicaciones y el criterio, y las ordena de 
+//acuerdo a eso, de nuevo, revisar si se puede hacer flexible, sino un sort por criterio
+
+// getAll devuelve todas las publicaciones(checkear con database)
+const getAll = async () => {
+    return await Publication.findAll({
+        include: [{
+            model: Property,
+            where: {
+                attributes: [],
+            }
+        },
+       /*  {
+            model: Report,
+            attributes:['name'],
+            where: {
+                attributes: [],
+            }
+        },
+        {
+            model: TypeOfProp,
+            attributes:['name'],
+            where: {
+                attributes: [],
+            }
+        },
+        {
+            model: City,
+            attributes:['name'],
+            where: {
+                attributes: [],
+            }
+        },
+        {
+            model: TypeOfProp,
+            attributes:['name'],
+            where: {
+                attributes: [],
+            }
+        }, */
+        ]
+    });
+};
+module.exports(getAll)
