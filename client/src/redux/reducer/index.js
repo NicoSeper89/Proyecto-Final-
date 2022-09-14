d319f9526b8c4988dbfb59f5a9c6423a5eb84b3c
TomasTinto1234 <tititinto@gmail.com>
2022-09-14 14:19:10 -0300

    actiones y reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,7 +1,22 @@
-const initialState = {};
+const initialState = {
+
+  detail:{}
+};
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
+
+    case "GET_DETAIL":
+      return {
+        ...state,
+        detail: action.payload
+      }
+
+      case "CLEAN":
+        return{
+          ...state,
+          detail:{}
+        }
     default:
       return state;
   }
