55967b805009917b807a894b83e7123208be82e6
ELJG <jose23122009@gmail.com>
2022-09-19 08:40:10 -0300

    registrar en proceso

--- client/src/components/Check_in/Check_in.jsx
+++ client/src/components/Check_in/Check_in.jsx
@@ -6,15 +6,37 @@ import {
   InputGroup,
   Button,
   InputRightElement,
-  Stack,
-  Checkbox,
+  Radio,
+  HStack,
+  RadioGroup,
+  Alert,
+  AlertIcon
 } from "@chakra-ui/react";
 import logoImg from "../../Image/Logo LookHouse.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+var gmail = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{3})$/ //ragex para validar gmail
 
 const NewUser = () => {
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  const [datos, setDatos] = useState({ NDU: "", gmail: "", contraseña: "", TDU: "" })
+
+  const changes = (e) => {
+    setDatos({ ...datos, [e.target.name]: e.target.value })
+  }
+  const createUser = () => {
+  console.log(datos)
+  setDatos({ NDU: "", gmail: "", contraseña: "", TDU: "" })
+  
+  }
+  
+
+  var si_no = true
+  if(gmail.test(datos.gmail) && datos.NDU.length > 5 && datos.contraseña.length > 8 && datos.TDU !== "")  si_no = false
+  else si_no = true
   return (
     <Box>
       <Box className={style.containerNav}>
@@ -30,22 +52,22 @@ const NewUser = () => {
 
       <form className={style.form}>
         <Box className={style.titulo}>
-          <h1>Crear Usuario</h1>
+          <h1>Crear Usuario:</h1>
         </Box>
 
         <Box className={style.input}>
-          <label htmlFor="">Nombre</label>
-          <Input placeholder="Nombre..." />
+          <label htmlFor="">Nombre De Usuario</label>
+          <Input placeholder="Nombre..." name="NDU" onChange={changes} value={datos.NDU} />
         </Box>
         <Box className={style.input}>
           <label htmlFor="">E-mail</label>
-          <Input placeholder="E-mail..." />
+          <Input placeholder="E-mail..." name="gmail" onChange={changes} value={datos.gmail} />
         </Box>
 
         <Box className={style.input}>
           <label htmlFor="">Contraseña</label>
           <InputGroup size="md">
-            <Input pr="4.5rem" type={show ? "text" : "password"} placeholder="Contraseña.." />
+            <Input pr="4.5rem" type={show ? "text" : "password"} placeholder="Contraseña.." name="contraseña" onChange={changes} value={datos.contraseña} />
             <InputRightElement width="4.5rem">
               <Button h="1.75rem" size="sm" onClick={handleClick}>
                 {show ? "Hide" : "Show"}
@@ -55,18 +77,19 @@ const NewUser = () => {
         </Box>
 
         <Box className={style.titulo}>
-          <label htmlFor="">Tipo De Usuario</label>
+          <label htmlFor="">Tipo De Usuario:</label>
         </Box>
 
         <Box className={style.input}>
-          <Stack spacing={5} direction="row">
-            <Checkbox colorScheme="red" defaultChecked>
-              Propietario
-            </Checkbox>
-            <Checkbox colorScheme="green" defaultChecked>
-              Inquilino
-            </Checkbox>
-          </Stack>
+          <RadioGroup defaultValue='Itachi'>
+            <HStack spacing='24px' onChange={changes}>
+              <Radio value='Inquilino' name="TDU">Inquilino</Radio>
+              <Radio value='Propietario' name="TDU">Propietario</Radio>
+            </HStack>
+          </RadioGroup>
+        </Box>
+        <Box className={style.btn}>
+          <Button colorScheme='blue' onClick={createUser} disabled={si_no}>Crear</Button>
         </Box>
       </form>
     </Box>
