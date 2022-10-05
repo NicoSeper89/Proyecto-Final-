76f8de9a95e715637190ffd095e74050a7ba89cd
MelHellrigl <100803744+MelHellrigl@users.noreply.github.com>
2022-10-05 17:51:56 -0300

    Update Help.jsx
    Se agregó información sobre escribanos

--- client/src/components/Help/Help.jsx
+++ client/src/components/Help/Help.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBarForms from "../NavBar/NavBarForms";
 import Footer from "../Footer/Footer";
 // import style from "./Help.module.css";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Text } from "@chakra-ui/react";
 
 export default function Help() {
   return (
@@ -15,15 +15,17 @@ export default function Help() {
         textAlign={"justify"}
         p={"50px"}
         paddingTop={"100px"}
+        paddingBottom={"100px"}
         // className={style.containerText}
       >
         <Text as="b" textTransform={"uppercase"} fontSize="2xl">
           Ayuda
         </Text>
+        <br />
         <Text as="samp" fontSize="xl">
           ¿Cómo me registro en LookHouse?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           El registro lo podes realizar muy facilmente ingresando con tu cuenta de Google, o
           registrándote con tu email. Y así es como podés formar parte de nuestra comunidad!
         </Text>
@@ -31,12 +33,12 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           ¿Cómo puedo realizar una publicación?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Si sos propietario, podes acceder al formulario de creación desde el menu de opciones
           desplegable. En el mismo deberás rellenar los datos de tu propiedad, podrás agregar una
           descripción con tus palabras y adjuntar las imagenes de la misma.
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Recorda de que los datos e imagenes que ingresaste se correspondan con la propiedad, ya
           que de lo contrario tu publicación podra ser reportada y eso bajara tu clasificación
           general como propietario.
@@ -45,7 +47,7 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           ¿Cómo puedo editar o borrar una publicación?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Para editar una publicación dirigite a la lista de todas tus publicaciones en tu perfil,
           selecciona la publicación que deseas modificar y abri su detalles y desde ahi podes acceder 
           a los botones de edición o borrado.
@@ -56,7 +58,7 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           ¿Qué es una publicación destacada?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Las publicaciones destacadas son publicaciones que aparecen en un sector especial y más visible de la página,
           aumentando asi su visibilidad frente a posibles inquilinos.
         </Text>
@@ -64,7 +66,7 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           ¿Cómo destaco mi publicación?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Luego de crear una publicación se te dará la opción de destacarla, al seleccionarla te redirigirá
           a la páginade mercadopago, completá con tus datos y finalizá la compra. Cuando vuelvas a la página principal
           ya podrás visualizarla entre los destacados. 
@@ -73,7 +75,7 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           No destaqué mi publicación cuando la creé y ahora quiero hacerlo ¿qué hago?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           No te preocupes, en tu perfil figura una lista de todas tus publicaciones, desde donde vas a tener
           la opción de destacar cualquiera de ellas, en cualquier momento.
         </Text>
@@ -81,9 +83,26 @@ export default function Help() {
         <Text as="samp" fontSize="xl">
           ¿Cómo me contacto con el dueño de una propiedad que me interesa?
         </Text>
-        <Text style={{ textIndent: 20 }} w={"400px"} h={"100%"}>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
           Dentro del detalle de cada publicación figuran los datos del dueño de la misma. Entre ellos encontraras
           los medios de contacto que proveyó.
+        </Text>
+        <br />
+        <Text as="samp" fontSize="xl">
+          ¿Hay información sobre escribanos y contratos?
+        </Text>
+        <Text style={{ textIndent: 20 }} w={"600px"} h={"100%"}>
+          Te facilitamos información sobre escribanas y escribanos matriculados que pueden guiarte
+          en el proceso.
+          <Link
+            href={
+              "https://www.argentina.gob.ar/interior/asuntos-tecnicos-de-fronteras/solicitud-de-previa-conformidad/listado-de-escribanas-y-escribanos"
+            }
+          >
+            <Button size="xs" ml={"15px"} colorScheme="teal" variant="outline">
+              +info
+            </Button>
+          </Link>
         </Text>
       </Flex>
       <Footer />
